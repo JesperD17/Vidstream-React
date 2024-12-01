543de7d68d27dcdb823d758210ecb52f323c333a
Jesper321 <drentj7@gmail.com>
2024-12-01 16:05:45 +0100

    icons styling header

--- vidstream-react/src/componants/header/header.jsx
+++ vidstream-react/src/componants/header/header.jsx
@@ -23,7 +23,9 @@ function Header() {
   }
 
   // function for screen width to shw 'links a' on specific width and the nuse menuActive under specific width. 
-
+  function screenWidth() {
+    
+  }
   return (
     <>
     {/* Logo's */}
@@ -57,14 +59,16 @@ function Header() {
 
         {/* Search bar */}
         <div className="full_search_bar">
+          <i id="searchIcon1"
+          className={"bx " + (!searchActive ? 'bx-search bx-tada' : 'bxs-search')} onClick={searchStyle}></i>
+        {searchActive &&
           <div id="search">
             <input type="text" name="search-field" placeholder="Zoeken..." id="search-field" className="blink search-field"/>
-              <i id="searchIcon1"
-              className={"bx " + (!searchActive ? 'bx-search bx-tada' : 'bxs-search')} onClick={searchStyle}></i>
               
               <i id="IconNoClick" className="bx bx-search bx-tada"></i>
             {/* </input> */}
           </div>
+            } 
         </div>
         {/* End search bar */}
       </div>
