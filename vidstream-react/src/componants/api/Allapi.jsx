d0490791bdb4764f8a327b59bec81095547c4285
Jesper321 <drentj7@gmail.com>
2024-12-26 17:53:55 +0100

    Finished skeleton cards. Finished cards.

--- vidstream-react/src/componants/api/Allapi.jsx
+++ vidstream-react/src/componants/api/Allapi.jsx
@@ -7,7 +7,6 @@ import Skeleton from '../skeleton/skeleton';
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 // using Swr for fetching api
 function Homepage() {
-console.log("aaaaaa")
   const {
     data: allMovies,
     error,
@@ -23,31 +22,75 @@ console.log("aaaaaa")
 
   // Handles error and loading/Skeleton.
   if (error) return <div className='failed'>failed to load</div>;
-  if (isValidating) return <Skeleton />; 
+  if (isValidating | !allMovies) return <Skeleton />; 
  
   
-  // console logging api etc.
+  // console logging api
   console.log(allMovies);
-  console.log(allMovies.latestMovies.length);
   
-  // repeating the card div structure to the amount of movies in api.
-  const moviesLength = allMovies.latestMovies.length;
-  const repeatCardDivs = new Array(moviesLength).fill(null);
-
+// repeating the card div structure to the amount of movies in api.
+const trendingMoviesLength = allMovies.trending.movies.length;
+const repeatTrendingMoviesCardDivs = new Array(trendingMoviesLength).fill(null);
 // if a string has "?", function changes it to empty string.
-let ratings = []; // making an object for updated strings.
+let ratingsTrendingMovies = [] // making an object for updated strings.
+var trendingMovieRating; 
+  for (let i = 0; i < allMovies.trending.movies.length; i++) {
+    trendingMovieRating = allMovies.trending.movies[i].stats.rating;
+
+    if(trendingMovieRating == "?") {
+      trendingMovieRating = ""; // empties string
+    } // sends the new strings to the object
+    ratingsTrendingMovies.push(trendingMovieRating);
+  }
+
+const trendingSeriesLength = allMovies.trending.tvSeries.length;
+const repeatTrendingSeriesCardDivs = new Array(trendingSeriesLength).fill(null);
+let ratingsTrendingSeries = [] 
+var trendingSerieRating; 
+  for (let i = 0; i < allMovies.trending.tvSeries.length; i++) {
+    trendingSerieRating = allMovies.trending.tvSeries[i].stats.rating;
+
+    if(trendingSerieRating == "?") {
+      trendingSerieRating = "";
+    } 
+    ratingsTrendingSeries.push(trendingSerieRating);
+  }
+
+const moviesLength = allMovies.latestMovies.length;
+const repeatMovieCardDivs = new Array(moviesLength).fill(null);
+let ratingsLatestMovies = [];
 var movieRating;
   for (let i = 0; i < allMovies.latestMovies.length; i++) {
     movieRating = allMovies.latestMovies[i].stats.rating;
 
     if(movieRating == "?") {
-      movieRating = ""; // empties string
-    } // sends the new strings to the object
-    ratings.push(movieRating);
+      movieRating = ""; 
+    } 
+    ratingsLatestMovies.push(movieRating);
   }
 
+const seriesLength = allMovies.latestTvSeries.length;
+const repeatSerieCardDivs = new Array(seriesLength).fill(null);
+
+let ratingsLatestTvSeries = []
+var serieRating; 
+  for (let i = 0; i < allMovies.latestTvSeries.length; i++) {
+    serieRating = allMovies.latestTvSeries[i].stats.rating;
+
+    if(serieRating == "?") {
+      serieRating = "";
+    }
+    ratingsLatestTvSeries.push(serieRating);
+  }
+
+
+  
   return (
     <>
+      <div className="Titles_wrapper">
+        <div className="MainTitles">Most trending movies & series.</div>
+        <div className="See_all_Links">See all. <i className='bx bx-right-arrow-alt'></i></div>
+      </div>
       <div 
         className='allCards'
         ref={containerRef}
@@ -57,7 +100,68 @@ var movieRating;
         onMouseLeave={handleMouseUpOrLeave}
       >
 
-        {repeatCardDivs.map((_, number) => (
+        {repeatTrendingMoviesCardDivs.map((_, number) => (
+          
+          <div className="card"
+          key={number}>
+            <div className="banner">
+              <img src={allMovies.trending.movies[number].poster} draggable="false" />
+            </div>
+            <div className="card_info">
+              <div className="titel">{allMovies.trending.movies[number].title} </div>
+              <div className="card_info_inner">
+                <div className="review">{ratingsTrendingMovies[number]} <i className='bx bxs-star'></i></div> 
+                <div className="duration">{allMovies.trending.movies[number].stats.duration}</div>
+                <div className="year">{allMovies.trending.movies[number].stats.year}</div>
+              </div>
+            </div>
+          </div>
+
+        ))}
+      </div>
+      <div 
+        className='allCards'
+        ref={containerRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
+      >
+
+        {repeatTrendingSeriesCardDivs.map((_, number) => (
+          
+          <div className="card"
+          key={number}>
+            <div className="banner">
+              <img src={allMovies.trending.tvSeries[number].poster} draggable="false" />
+            </div>
+            <div className="card_info">
+              <div className="titel">{allMovies.trending.tvSeries[number].title} </div>
+              <div className="card_info_inner">
+                <div className="review">{ratingsTrendingSeries[number]} <i className='bx bxs-star'></i></div> 
+                <div className="year">{allMovies.trending.tvSeries[number].stats.seasons}</div>
+              </div>
+            </div>
+          </div>
+
+        ))}
+      </div>
+
+
+      <div className="Titles_wrapper">
+        <div className="MainTitles">Newest movies.</div>
+        <div className="See_all_Links">See all. <i className='bx bx-right-arrow-alt'></i></div>
+      </div>
+      <div 
+        className='allCards'
+        ref={containerRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
+      >
+
+        {repeatMovieCardDivs.map((_, number) => (
           
           <div className="card"
           key={number}>
@@ -66,9 +170,45 @@ var movieRating;
             </div>
             <div className="card_info">
               <div className="titel">{allMovies.latestMovies[number].title} </div>
-              <div className="review">{ratings[number]}</div> 
-              <div className="duration">{allMovies.latestMovies[number].stats.duration}</div>
-              <div className="year">{allMovies.latestMovies[number].stats.year}</div>
+              <div className="card_info_inner">
+                <div className="review">{ratingsLatestMovies[number]} <i className='bx bxs-star'></i></div> 
+                <div className="duration">{allMovies.latestMovies[number].stats.duration}</div>
+                <div className="year">{allMovies.latestMovies[number].stats.year}</div>
+              </div>
+            </div>
+          </div>
+
+        ))}
+      </div>
+
+
+
+      <div className="Titles_wrapper">
+        <div className="MainTitles">Newest Series.</div>
+        <div className="See_all_Links">See all. <i className='bx bx-right-arrow-alt'></i></div>
+      </div>  
+      <div 
+        className='allCards'
+        ref={containerRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
+      >
+
+        {repeatSerieCardDivs.map((_, number) => (
+          
+          <div className="card"
+          key={number}>
+            <div className="banner">
+              <img src={allMovies.latestTvSeries[number].poster} draggable="false" />
+            </div>
+            <div className="card_info">
+              <div className="titel">{allMovies.latestTvSeries[number].title} </div>
+              <div className="card_info_inner">
+                <div className="review">{ratingsLatestTvSeries[number]} <i className='bx bxs-star'></i></div> 
+                <div className="seasons">{allMovies.latestTvSeries[number].stats.seasons}</div>
+              </div>
             </div>
           </div>
 
--- vidstream-react/src/componants/skeleton/skeleton.jsx
+++ vidstream-react/src/componants/skeleton/skeleton.jsx
@@ -6,73 +6,70 @@ import Drag from '../global functions/draggableItems';
 
 function Skeleton() {
 
-// number of cards preloaded
-    const totalItems = 20;
-    const items = new Array(totalItems).fill(null);
+    // number of cards preloaded
+    const amountOfCards = 20;
+    const totalCards = new Array(amountOfCards).fill(null);
 
+    const amountOfStructures = 4;
+    const totalStructures = new Array(amountOfStructures).fill(null);
 
-// importing drag function
-const { containerRef, handleMouseDown, handleMouseMove, handleMouseUpOrLeave } = Drag();
+    // importing drag function
+    const { containerRef, handleMouseDown, handleMouseMove, handleMouseUpOrLeave } = Drag();
 
-Drag()
+    Drag()
     return (
-        <>  
-            <div className={"allCards"}
-            ref={containerRef}
-                onMouseDown={handleMouseDown}
-                onMouseMove={handleMouseMove}
-                onMouseUp={handleMouseUpOrLeave}
-                onMouseLeave={handleMouseUpOrLeave}
-            >
-                {items.map((any, number) => (
-                    <div className={"card " + number} 
-                    key={number}
-                    style={{background: "white"}}
-                    >
-                        <div className="banner">
-                            <img 
-                                onerror="display='none'" 
-                                draggable="false"
-                                style={{
-                                    animation: "skeleton-loading 1s linear infinite alternate",
-                                    color: "transparent"
-                                }}
-                            /> 
+        <>
+        {totalStructures.map((_, structures) => (
+            <div className="allSkeleton_Cards"
+            key={structures}>
+                <div className="Titles_wrapper">
+                    <div className="MainTitles"
+                    style={{ animation: "skeleton-loading 1s linear infinite alternate", color: "transparent" }}
+                    >Title of mainTitles.</div>
+                    <div className="See_all_Links"
+                    style={{ animation: "skeleton-loading 1s linear infinite alternate", color: "transparent" }}
+                    >Links of See_all_Links. <i className='bx bx-right-arrow-alt'></i></div>
+                </div>
+                <div className={"allCards"}
+                    ref={containerRef}
+                    onMouseDown={handleMouseDown}
+                    onMouseMove={handleMouseMove}
+                    onMouseUp={handleMouseUpOrLeave}
+                    onMouseLeave={handleMouseUpOrLeave}
+                >
+                    {totalCards.map((_, number) => (
+                        <div className={"card " + number}
+                            key={number}
+                            style={{ background: "white" }}>
+                            <div className="banner">
+                                <img
+                                    draggable="false"
+                                    style={{ animation: "skeleton-loading 1s linear infinite alternate", color: "transparent" }}
+                                />
+                            </div>
+                            <div className="card_info"
+                                style={{ gap: "10px", display: "flex", flexDirection: "column" }}>
+                                <div className="titel"
+                                    style={{ animation: "skeleton-loading 1s linear infinite alternate", color: "transparent" }}
+                                >titel {number}</div>
+
+                                <div className="card_info_inner">
+                                    <div className="review"
+                                        style={{ animation: "skeleton-loading 1s linear infinite alternate", color: "transparent" }}
+                                    >review</div>
+                                    <div className="duration"
+                                        style={{ animation: "skeleton-loading 1s linear infinite alternate", color: "transparent" }}
+                                    >duration</div>
+                                    <div className="year"
+                                        style={{ animation: "skeleton-loading 1s linear infinite alternate", color: "transparent" }}
+                                    >year</div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card_info"
-                        style={{
-                            gap: "10px",
-                            display: "flex",
-                            flexDirection: "column"
-                        }}>
-                            <div className="titel"
-                            style={{
-                                animation: "skeleton-loading 1s linear infinite alternate",
-                                color: "transparent"
-                            }}
-                            >titel {number}</div>
-                            <div className="review"
-                            style={{
-                                animation: "skeleton-loading 1s linear infinite alternate",
-                                color: "transparent"
-                            }}
-                            >review</div>
-                            <div className="duration"
-                            style={{
-                                animation: "skeleton-loading 1s linear infinite alternate",
-                                color: "transparent"
-                            }}
-                            >duration</div>
-                            <div className="year"
-                            style={{
-                                animation: "skeleton-loading 1s linear infinite alternate",
-                                color: "transparent"
-                            }}
-                            >year</div>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
+        ))}
         </>
     );
 }
