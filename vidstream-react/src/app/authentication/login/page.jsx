0a2e4994918308ed32bb470257710cbe5be12d26
Jesper321 <drentj7@gmail.com>
2025-02-02 00:05:23 +0100

    Installed mysql2, made a connection with database. Attempting to make a check on the loginPage with submitHandler.

--- vidstream-react/src/app/authentication/login/page.jsx
+++ vidstream-react/src/app/authentication/login/page.jsx
@@ -1,12 +1,37 @@
 "use client"
 
+import { useEffect } from "react"
+
+function submitHandler(event) {
+  event.preventDefault();
+  const { response } = GET()
+  console.log("submitted")
+  console.log(response)
+}
+
+function GET() {
+  useEffect(() => {
+    let response;
+    const fetchData = async () => {
+      try {
+        const { data } = await fetch('../../api/mail')
+        console.log(data)
+        response = data.json()
+        console.log(response, "aaaa")
+  
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+    return response;
+  }, [])
+}
+
 export default function LoginPage() {
-  function submitHhandler() {
-    console.log("submitted")
-  }
- 
+  
   return (
-    <form onSubmit={submitHhandler}>
+    <form onSubmit={submitHandler}>
       <input type="email" name="email" placeholder="Email" required />
 
       <input type="password" name="password" placeholder="Password" required />
